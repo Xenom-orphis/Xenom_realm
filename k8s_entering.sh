3c545895aa05c509f4f2b5ae8c6b7bec77ce9d04
hainakus <djhainakosurge@gmail.com>
2024-11-12 14:18:36 +0000

    Update k8s_entering.sh

--- k8s_entering.sh
+++ k8s_entering.sh
@@ -37,7 +37,7 @@ sudo sysctl --system
 
 echo "Installing Kubernetes apt repository..."
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg --allow-downgrades
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
